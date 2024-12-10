66bc302d666f04effb3fea68916adf7b8909e8de
Bashar Jaan Khan <basharjaankhan@gmail.com>
2024-12-10 22:31:46 +0100

    fix: remove commented code

--- config/config.go
+++ config/config.go
@@ -349,42 +349,6 @@ func (c *Crawl) BootstrapEnodesV4() ([]*enode.Node, error) {
 
 func (c *Crawl) BootstrapBitcoinEntries() ([]ma.Multiaddr, error) {
 
-	// go func(dnsSeeds []string) []string {
-	// 	wait := sync.WaitGroup{}
-	// 	results := make(chan []net.IP)
-
-	// 	for _, seed := range dnsSeeds {
-	// 		wait.Add(1)
-	// 		go func(address string) {
-	// 			defer wait.Done()
-	// 			ips, err := net.LookupIP(address)
-	// 			if err != nil {
-	// 				logger.Warningf("Failed to resolve %s: %v", address, err)
-	// 				return
-	// 			}
-	// 			logger.Debugf("Resolved %d seeds from %s.", len(ips), address)
-	// 			results <- ips
-	// 		}(seed)
-	// 	}
-
-	// 	go func() {
-	// 		wait.Wait()
-	// 		close(results)
-	// 	}()
-
-	// 	seeds := []string{}
-	// 	for ips := range results {
-	// 		for _, ip := range ips {
-	// 			seeds = append(seeds, net.JoinHostPort(ip.String(), chaincfg.MainNetParams.DefaultPort))
-	// 		}
-	// 	}
-
-	// 	logger.Infof("Resolved %d seed nodes from %d DNS seeds.", len(seeds), len(dnsSeeds))
-
-	// 	// Note that this will likely include duplicate seeds. The crawler deduplicates them.
-	// 	return seeds
-	// }()
-
 	addrInfos := make([]ma.Multiaddr, 0, len(c.BootstrapPeers.Value()))
 
 	for _, maddrStr := range c.BootstrapPeers.Value() {
